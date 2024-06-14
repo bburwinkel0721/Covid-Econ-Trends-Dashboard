e1be756bff822d0ac72c7a1ae7be97366c3fc588
bburwinkel <burwinkelben1@gmail.com>
2024-06-14 13:01:31 -0400

    got the drop down working and panel working

--- script.js
+++ script.js
@@ -112,95 +112,183 @@ function onEachFeature2(feature, layer) {
                     '<p>Census Area: ' + feature.properties.CENSUSAREA + ' mi<sup>2</sup></p>');
 }
 
+function buildStatedata(sample) {
+    d3.json(stateUrl).then((data) => {
+  
+    // get the metadata field
+    const metaDataField = data.features
+
+    // Filter the metadata for the object with the desired sample number
+    let desiredSample = metaDataField.filter(object => object.properties.name == sample)
+    
+    // Use d3 to select the panel with id of `#sample-metadata`
+    let panel = d3.select('#sample-metadata')
+
+    // Use `.html("") to clear any existing metadata
+    panel.html("")
+
+    let newThing = desiredSample[0].properties
+
+    Object.entries(newThing).forEach(([key, value]) => {
+            panel.append("p")
+                .text(`${key.toUpperCase()}: ${value}`)
+                .style('opacity', 0)
+                .transition()
+                .duration(500)
+                .style('opacity', 1);
+        });
+      });
+    
+  }
+
+  function init() {
+    d3.json(stateUrl).then((data) => {
+      console.log(data)
+      // Get the names field
+      const namesField = []
+      for (state of data.features){
+        namesField.push(state.properties.name)
+      }
+  
+      // Use d3 to select the dropdown with id of `#selDataset`
+      let dropdownMenu = d3.select("#selDataset")
+  
+      // Use the list of sample names to populate the select options
+      // Hint: Inside a loop, you will need to use d3 to append a new
+      // option for each sample name.
+      dropdownMenu.selectAll("option")
+          .data(namesField)
+          .enter()
+          .append("option")
+          .text(d => d)
+          .attr("value", d => d);
+  
+      // Get the first sample from the list
+      const firstName = namesField[0]
+  
+      // Build charts and metadata panel with the first sample
+      buildStatedata(firstName)
+    });
+}
+
+// Function for event listener
+function optionChanged(newSample) {
+    // Build charts and metadata panel each time a new sample is selected
+    buildStatedata(newSample);
+    
+}
+
+init()
 
 d3.json(stateUrl).then(data =>{
 
-geojson2 = L.geoJson(usCounties, {
-    style: style,
-    onEachFeature: onEachFeature2
-}).addTo(map);
+    geojson2 = L.geoJson(usCounties, {
+        style: style,
+        onEachFeature: onEachFeature2
+    }).addTo(map);
+
     geojson = L.geoJson(data, {
         style: style,
         onEachFeature: onEachFeature
     }).addTo(map);
-    console.log(data)
+    
     // geojson = L.geoJson(stateData, {
-        //     style: style,
-        //     onEachFeature: onEachFeature
-        // }).addTo(map);
-        
-        
-        // var info = L.control();
-        
-        info.onAdd = function (map) {
-            this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-            this.update();
-            return this._div;
+    //     style: style,
+    //     onEachFeature: onEachFeature
+    // }).addTo(map);
+    
+    
+    // var info = L.control();
+    
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
+    
+    // method that we will use to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>US Map</h4>' +  (props ?
+            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+            : 'Hover over a state');
         };
         
         // method that we will use to update the control based on feature properties passed
-        info.update = function (props) {
-            this._div.innerHTML = '<h4>US Map</h4>' +  (props ?
-                '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-                : 'Hover over a state');
-            };
-            
-            // method that we will use to update the control based on feature properties passed
-            info.update2 = function (name) {
-                this._div.innerHTML = '<h4>US Map</h4>' +  (name ?
-                    '<b>' + name + '</b><br />' + name + ' people / mi<sup>2</sup>'
-                    : 'Hover over a county');
-                };
-                
-                info.addTo(map);
-                
-                let baseMaps = {
-                    "Street Map": tiles
-                };
-                
-                const overlayMaps = {
-                    States: geojson,
-                    Counties: geojson2,
-                };
-                
-                // Create a layer control that contains our baseMaps.
-                let layerControl = L.control.layers(baseMaps, overlayMaps, {
-                    collapsed: false,
-                });
-                layerControl.addTo(map);
-                
-                var legend = L.control({position: 'bottomright'});
-                
-                legend.onAdd = function (map) {
-                    
-                    var div = L.DomUtil.create('div', 'info legend'),
-                    grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-                    labels = [];
-                    
-                    // loop through our density intervals and generate a label with a colored square for each interval
-                    for (var i = 0; i < grades.length; i++) {
-                        div.innerHTML +=
-                        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-                    }
-                    
-                    return div;
-                };
-                
-                legend.addTo(map);
-                
-                d3.select('#title')
-                .style('background-color', 'rgb(0, 119, 190)')
-                .style("box-shadow", "10px 10px 5px grey")
-                .style("border", "1px solid black")
-                .style("border-radius", "15px");
-                
-                d3.select('#map')
-                .style('background-color', 'white')
-                .style("box-shadow", "10px 10px 5px grey")
-                .style("border", "1px solid black")
-                .style("border-radius", "15px");
-            })
+    info.update2 = function (name) {
+        this._div.innerHTML = '<h4>US Map</h4>' +  (name ?
+            '<b>' + name + '</b><br />' + name + ' people / mi<sup>2</sup>'
+            : 'Hover over a county');
+        };
+        
+    info.addTo(map);
+    
+    let baseMaps = {
+        "Street Map": tiles
+    };
+    
+    const overlayMaps = {
+        States: geojson,
+        Counties: geojson2,
+    };
+    
+    // Create a layer control that contains our baseMaps.
+    let layerControl = L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false,
+    });
+    layerControl.addTo(map);
+    
+    var legend = L.control({position: 'bottomright'});
+    
+    legend.onAdd = function (map) {
+        
+        var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+        
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        
+        return div;
+    };
+    
+    legend.addTo(map);
+    
+    d3.select('#title')
+    .style('background-color', 'rgb(0, 119, 190)')
+    .style("box-shadow", "10px 10px 5px grey")
+    .style("border", "1px solid black")
+    .style("border-radius", "15px");
+    
+    d3.select('#map')
+    .style('background-color', 'white')
+    .style("box-shadow", "10px 10px 5px grey")
+    .style("border", "1px solid black")
+    .style("border-radius", "15px");
+
+    // Styling for the top ID number block
+    let idPanel = d3.select('.card')
+    idPanel.transition().duration(500)
+        .style('background-color', 'red')
+        .style("box-shadow", "10px 10px 5px grey")
+        .style("border", "1px solid black")
+
+    // Styling for the demographic info header
+    let panelHeader = d3.select('.card-header')
+    panelHeader.transition().duration(500)
+        .style('background-color', 'green')
+        .style("border-radius", "15px");
+
+    // Styling for the demographic info metadata block
+    let metaBlock = d3.select('.card-primary')
+    metaBlock.transition().duration(500)
+        .style("box-shadow", "10px 10px 5px grey")
+        .style("border", "1px solid black")
+        .style("border-radius", "15px");
+})
                 // stateUrl = 'http://127.0.0.1:5000/api/v1.0/states'
 
 // var map = L.map('map').setView([37.8, -96], 4);
